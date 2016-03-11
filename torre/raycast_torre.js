bd70622ea57f2401a08cad5deab78b02ad065331
Gina <ginastavropoulou@gmail.com>
2016-03-11 11:29:36 +0100

    major changes in torre.html and raycast_torre.html

--- torre/raycast_torre.js
+++ torre/raycast_torre.js
@@ -2,133 +2,118 @@
 var raycaster = new THREE.Raycaster();
 
 
-
-
 function onDocumentMouseDown( event )
-{   //event.preventDefault();
-	//https://it.wikipedia.org/wiki/Torre_Aquila
-	//drawImage();
-	var camera = viewer.camera;
-
-	// the following line would stop any other event handler from firing
-	// (such as the mouse's TrackballControls)
-	// event.preventDefault();
-
-	console.log("Click.");
-
-
+{   var camera = viewer.camera;
 	// update the mouse variable
 	var mouse = { x : 0 , y : 0 };
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-
-	// mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	// mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-
 	// update the picking ray with the camera and mouse position
 	raycaster.setFromCamera( mouse, camera );
 
-	// create an array containing all objects in the scene with which the ray intersects
-	var intersects = raycaster.intersectObjects( [plane] );
+	//check first if the "click" was on a poi and create an array with them
+	var intersects_poi = raycaster.intersectObjects( hotspots.children );
+		// if there is an intersection with a poi...
+		if ( intersects_poi.length > 0 ) {
+			//...and if the first intersected poi (in space) is visible...
+			if (intersects_poi[ 0 ].object.visible==true) {
+				//..then check which poi is it and open to a new window the appropriate link
+				if (intersects_poi[ 0 ].object.userData.name=="Jan")
+					{window.open("../../web_pages/january.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Feb")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Apr")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="May")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Jun")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Jul")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Aug")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Sep")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Oct")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Nov")
+					{window.open("../../web_pages/index.html")}
+				if (intersects_poi[ 0 ].object.userData.name=="Dec")
+					{window.open("../../web_pages/index.html")}
+			}
+		}
 
+
+	// create an array with intersections with planes
+	var intersects = raycaster.intersectObjects( planes.children );
 	// if there is one (or more) intersections
-	if ( intersects.length > 0 )
-	{
-		console.log("Hit right @ " + toString( intersects[0].point ) );
-		//window.open("../../torre/examples/torre.html")
-		// change the color of the closest face.
-		//intersects[0].object.callback();
-		intersects[ 0 ].face.color.setRGB( 0.8 * Math.random() + 0.2, 0, 0 );
-		intersects[ 0 ].object.geometry.colorsNeedUpdate = true;
-		// window.alert("Questo Ã¨ un esempio di alert incluso nello script.");
-		winW = ""+(screen.width / 2) -200 ;
-		winH = ""+(screen.height / 2) -200 ;
-		// window.open("http://www.gazzetta.it", "_blank", " scrollbars=yes, resizable=yes, top=" + winH + ", left="+ winW +", width=400, height=400");
-		// window.open("", "MsgWindow", "resizable=yes", "top=500", "left=500", "width=100", "height=100");
+	if ( intersects.length > 0 ){	
+		// Set Default
+		planes.children.forEach(function( plane ) {plane.material.opacity=[0.3];});
+		hotspots.children.forEach(function( hotspot ) {hotspot.visible=false;});
+
+		//change the opacity of the intersected plane --> to demonstrate interaction
+		intersects[ 0 ].object.material.opacity=[0.5];
+
+		// Turn on the poi that has the same name as the intersected plane
+		hotspots.children.forEach(function( hotspot ) {if (hotspot.userData.name==intersects[ 0 ].object.userData.name){hotspot.visible=true; console.log("Clicked " +intersects[ 0 ].object.userData.name);}});
+
 	}
-	// else{
-	// 	viewer.scenePointCloud.remove(plane);
-	// }
+	else{
+		planes.children.forEach(function( plane ) {plane.material.opacity=[0.3];});
+		planes.children.forEach(function( plane ) {plane.visible=false;});
+		hotspots.children.forEach(function( hotspot ) {hotspot.visible=false;});
+	}		
+
+	
+
 
 }
+
+
 function onMouseMove( event ) {
 	var camera = viewer.camera;
 
 	// calculate mouse position in normalized device coordinates
-	// (-1 to +1) for both components
 	var mouse = { x : 0 , y : 0 };
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 	// update the picking ray with the camera and mouse position
 	raycaster.setFromCamera( mouse, camera );
 
-	// create an array containing all objects in the scene with which the ray intersects
-	var intersects = raycaster.intersectObjects( [plane] );
 
-	// if there is one (or more) intersections
-	if ( intersects.length > 0 )
-	{
-		console.log("Hit this @ " + toString( intersects[0].point ) );
-		// change the color of the closest face.
-		intersects[ 0 ].object.material.color.setRGB(1,0.3,1);
-		intersects[ 0 ].object.material.opacity=[0.3];
-		intersects[ 0 ].object.geometry.colorsNeedUpdate = true;
-		plane_hs.visible=true
-		plane_hs.rotation.y = viewer.camera.rotation.y;
-		console.log(viewer.camera.rotation.y);
+	//Find the intersections with POIs
+	var intersects_poi = raycaster.intersectObjects( hotspots.children );
+	// if there is one (or more) intersections with POIs...
+	if ( intersects_poi.length > 0 ){
+		// And the first one is visible...
+		if (intersects_poi[ 0 ].object.visible==true) {
+			//....then change (slightly) the scale and the opacity to demonstrate interaction/"clickability"
+			intersects_poi[ 0 ].object.scale.set(1.1,1.1,1.1);
+			intersects_poi[ 0 ].object.material.opacity=[1];
 
-	}
+		}}
+	//else return to default scale and opacity
+	else {hotspots.children.forEach(function( hotspot ) {hotspot.scale.set(1,1,1); hotspot.material.opacity=[poi_opacity];});}
+
+
+	//Find the intersections with planes
+	var intersects = raycaster.intersectObjects( planes.children );
+	// if there is one (or more) intersections with planes...
+	if ( intersects.length > 0 ){
+		//set all planes not visible...
+		planes.children.forEach(function( plane ) {plane.visible=false;});
+		// .. but turn the intersected one on!
+		intersects[0].object.visible = true ;
+		}
+
 	else {
-		plane.material.color.setRGB( 1,0,1);
-		plane.geometry.colorsNeedUpdate = true;
-		plane.material.opacity=[0];
-		plane_hs.visible=false
-		// plane2.material.color.setRGB( 1,0,1);
-		// plane2.geometry.colorsNeedUpdate = true;
-		// plane2.material.opacity=[0];
-	}
-	//plane.lookAt(camera.position);
+		//turn off all the planes when not hovered over
+		planes.children.forEach(function( plane ) {plane.visible=false;});			
+		}
+
+	// Make the POIs rotate towards the camera when moving  ---> If deactivated the rotation of the ROIs must be set in torre.html
+	hotspots.children.forEach(function( hotspot ) {hotspot.lookAt(camera.position);});
 
 }
-
-// function drawImage(){
-// 	var camera = viewer.camera;
-// 	// var scene = viewer.scenePointCloud;
-
-
-//     // var img = new THREE.MeshBasicMaterial({ map:THREE.ImageUtils.loadTexture('d.jpg') });
-//     // img.map.needsUpdate = true; //ADDED
-
-//     // // plane
-//     // var plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10),img);
-//     // plane.overdraw = true;
-
-
-//     // plane.rotation.y = (-Math.PI / 2);
-//     // plane.position.y = 10;
-//     // plane.position.z = 10;
-//     // plane.position.x = -10;
-// 	// plane.rotation.z = (-Math.PI / 2);
-
-//     viewer.scenePointCloud.add(plane);
-
-//     // add subtle ambient lighting
-//     // var ambientLight = new THREE.AmbientLight(0x555555);
-//     // scene.add(ambientLight);
-
-//     // add directional light source
-//     // var directionalLight = new THREE.DirectionalLight(0xffffff);
-//     // directionalLight.position.set(1, 1, 1).normalize();
-//     // scene.add(directionalLight);
-
-//     // create wrapper object that contains three.js objects
-//     // var three = {
-//     //     renderer: renderer,
-//     //     camera: camera,
-//     //     scene: scene,
-//     //     plane: plane
-//     // };
-//     // renderer.render(scene,camera);
-// }
